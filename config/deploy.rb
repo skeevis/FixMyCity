94ebee031ec23c8d4eb2014b63d6118b2666f56c
Zvi Band <zvi@skeevisarts.com>
2009-06-24 01:31:29 -0400

    Whats going on

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,11 +33,14 @@ namespace :deploy do
   desc "Restarting mod_rails with restart.txt and restarting god, workling and fetchmail"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
+    run "cd #{current_path} && mongrel_rails restart"
   end
 
   [:start].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do
+          run "cd #{current_path} && mongrel_rails restart"
+
 #      run "cd #{release_path} && rake asset:packager:build_all"
  #     run "god && god load #{current_path}/config/environments/#{rails_env}.god && god restart workling"
     end
